2bf0477d6a384499788e8bf02a287fcd05f8fdee
Gabriel <104459676+gabriel-ambrosio@users.noreply.github.com>
2025-05-08 19:31:20 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -4,8 +4,12 @@ import Livros
 import Tipos
 import Usuarios
 import Relatorios
+import Emprestimos
 import System.IO
 
+
+main :: IO ()
+main = laçoMenu [] [] [] [] []
 
 exibirOpçoes :: [String] -> IO ()
 exibirOpçoes opçoes = do
@@ -48,111 +52,115 @@ menu = do exibirOpçoes texto
     opMin = 1
     opMax = 7
 
-subMenu :: String -> Int -> Int -> IO Int
-subMenu str min max = do 
+subMenu :: [String] -> Int -> Int -> IO Int
+subMenu str min max = do
                          exibirOpçoes str
                          lerOpçao min max
 
 
-laçoSubMenu1 :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucoes] -> [Espera] -> IO ()
-laçoSubMenu1 u l e d ep  =  do
-                              opçao <- subMenu ["1 - Adicionar Livro",
-                                                "2 - Remover Livro",
-                                                "3 - Listar Livros",
-                                                "0 - Voltar"] 0 3
-                            case opçao of
-                              1 -> do titulo <- prompt "Digite o titulo do livro: "
-                                      autor <- prompt "Digite o autor do livro: "
+laçoSubMenu1 :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucao] -> [Espera] -> IO ()
+laçoSubMenu1 u l e d ep  = do
+                             opçao <- subMenu ["1 - Adicionar Livro", "2 - Remover Livro", "3 - Listar Livros", "0 - Voltar"] 0 3
+                             case opçao of
+                              1 -> do putStrLn "Digite o titulo do livro: "
+                                      titulo <- getLine
+                                      putStrLn "Digite o autor do livro: "
+                                      autor <- getLine
                                       ano <- prompt "Digite o ano do livro: "
                                       idLivro <- prompt "Digite o id do livro: "
-                                      let livro = Livro titulo autor ano idLivro
-                                      let novaLista = adicionarLivro livro l
-                                      validarAdicao livro novaLista
+                                      let novolivro = Livro titulo autor ano idLivro
+                                      validarAdicao novolivro l
+                                      let novaLista = adicionarLivro novolivro l
                                       laçoSubMenu1 u novaLista e d ep
                               2 -> do idLivro <- prompt "Digite o id do livro: "
-                                      removerLivro idLivro l
                                       validarRemocao idLivro l
+                                      let novaLista = removerLivro idLivro l
+                                      laçoSubMenu1 u novaLista e d ep
+                              3 -> do print l
                                       laçoSubMenu1 u l e d ep
-                              3 -> print l
-                              _ -> do laçoMenu l e d u ep
+                              _ -> laçoMenu u l e d ep
 
-laçoSubMenu2 :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucoes] -> [Espera] -> IO () 
+laçoSubMenu2 :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucao] -> [Espera] -> IO () 
 laçoSubMenu2  u l e d ep = do
-                             opçao <- subMenu ["1 - Adicionar Usuário",
-                                               "2 - Remover Usuário",
-                                               "3 - Listar Usuários",
-                                               "0 - Voltar"] 0 3
+                             opçao <- subMenu ["1 - Adicionar Usuário", "2 - Remover Usuário", "3 - Listar Usuários", "0 - Voltar"] 0 3
                              case opçao of
-                               1 -> do nome <- prompt "Digite o nome do usuário: "
-                                       matricula <- prompt "Digite a matricula do usuário: "
-                                       email <- prompt "Digite o email do usuário: "
-                                       let usuario = Usuario nome matricula email
-                                       let novaLista = adicionarLivro livro l
-                                       validarAdicao livro novaLista
-                                       laçoSubMenu1 u novaLista e d ep
+                               1 -> do putStrLn "Digite o nome do usuário: "
+                                       nome <- getLine
+                                       putStrLn "Digite a matricula do usuário: "
+                                       matricula <- getLine
+                                       putStrLn "Digite o email do usuário: "
+                                       email <- getLine
+                                       let novoUsuario = Usuario nome matricula email
+                                       validarAdicaoUsuario novoUsuario u
+                                       let novaLista = adicionarUsuario novoUsuario u
+                                       laçoSubMenu2 novaLista l e d ep
+                               2 -> do putStrLn "Digite a matricula do Usuario: " 
+                                       matricula <- getLine
+                                       validarRemocaoUsuario matricula u
+                                       let novaLista = removerUsuario matricula u
+                                       laçoSubMenu2 novaLista l e d ep
+                               3 -> do print u
+                                       laçoSubMenu2 u l e d ep
+                               _ -> laçoMenu u l e d ep
+
+laçoSubMenu3 :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucao] -> [Espera] -> IO ()
+laçoSubMenu3 u l e d ep = do
+                            opçao <- subMenu ["1 - Registrar empréstimos", "2 - Registrar devoluções", "3 - Listar livros emprestados e disponíveis", "4 - Lista de Espera", "0 - Voltar"] 0 3
+                            case opçao of
+                              1 -> do dia <- prompt "Digite o dia: "
+                                      mes <- prompt "Digite o mes: "
+                                      putStrLn "Digite o ano: "
+                                      anoStr <- getLine
+                                      let ano = read anoStr :: Integer
+                                      putStrLn "Digite o nome do usuário: "
+                                      nome <- getLine
+                                      putStrLn "Digite a matricula do usuário: "
+                                      matricula <- getLine
+                                      putStrLn "Digite o email do usuário: "
+                                      email <- getLine
+                                      putStrLn "Digite o titulo do livro: "
+                                      titulo <- getLine
+                                      putStrLn "Digite o autor do livro: "
+                                      autor <- getLine
+                                      ano <- prompt "Digite o ano do livro: "
+                                      idLivro <- prompt "Digite o id do livro: "
+                                      let usuario = Usuario nome matricula email
+                                      let livro = Livro titulo autor ano idLivro
+                                      let emprestimo = criarEmprestimo (dia,mes,ano) livro usuario
+                                      c <- validaEmprestimoIO emprestimo e
+                                      case c of
+                                        's' -> do
+                                            cadastrarEsperaIO usuario livro ep
+                                            let listaEspera = cadastrarEspera usuario livro ep
+                                            laçoSubMenu3 u l e d listaEspera
+                                        'n' -> laçoSubMenu3 u l e d ep
+                                        ' ' -> do
+                                            let novaListaE = registrarEmprestimo emprestimo e
+                                            laçoSubMenu3 u l novaListaE d ep
+                              2 -> do putStrLn ""
+                              3 -> do putStrLn ""
+                              4 -> do putStrLn ""
+                              _ -> laçoMenu u l e d ep
+
+laçoSubMenu4 :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucao] -> [Espera] -> IO ()
+laçoSubMenu4  u l e d ep = do
+                             opçao <- subMenu ["1 - Emprestimos ativos", "2 - Historico de Empréstimos", "3 - Lista de Espera", "0 - Voltar"] 0 3
+                             case opçao of
+                               1 -> do putStrLn ""
                                2 -> do putStrLn ""
                                3 -> do putStrLn ""
                                _ -> laçoMenu u l e d ep
 
-laçoSubMenu3 :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucoes] -> [Espera] -> IO ()
-laçoSubMenu3 u l e d ep = do
-                            opçao <- subMenu ["1 - Registrar empréstimos",
-                                              "2 - Registrar devoluções",
-                                              "3 - Listar livros emprestados e disponíveis",
-                                              "4 - Lista de Espera"
-                                              "0 - Voltar"] 0 3
-                            case opçao of
-                              1 -> do putStrLn ""
-                              2 -> do putStrLn ""
-                              3 -> do putStrLn ""
-                              _ -> laçoMenu u l e d ep
 
-laçoSubMenu4 :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucao] -> [Espera] -> IO ()
-laçoSubMenu4 u l e d ep = do
-                            opçao <- subMenu ["1 - Empréstimos ativos",
-                                              "2 - Histórico de Empréstimos por Usuário",
-                                              "3 - Lista de Espera Completa",
-                                              "4 - Livros Disponíveis",
-                                              "5 - Livros Emprestados",
-                                              "0 - Voltar"] 0 5
-                             case opçao of
-                               1 -> do 
-                                     putStrLn "Empréstimos ativos:"
-                                     print $ emprestimosAtivos e
-                                     laçoSubMenu4 u l e d ep
-                               2 -> do 
-                                     matricula <- prompt "Digite a matrícula do usuário: "
-                                     case encontraUsuario matricula u of
-                                     Just usuario -> do
-                                     putStrLn $ "Histórico de empréstimos para " ++ nome usuario ++ ":"
-                                     print $ historicoUsuario usuario e
-                                     Nothing -> putStrLn "Usuário não encontrado!"
-                                     laçoSubMenu4 u l e d ep
-                               3 -> do 
-                                     putStrLn "Lista de espera completa:"
-                                     print $ livrosComEspera (map (\esp -> (idLivro $ livroEsp esp, [usuarioEsp esp])) ep) l
-                                     laçoSubMenu4 u l e d ep
-                               4 -> do 
-                                     putStrLn "Livros disponíveis:"
-                                     print $ livrosDisponiveis l e
-                                     laçoSubMenu4 u l e d ep
-                               5 -> do 
-                                     putStrLn "Livros emprestados:"
-                                     print $ livrosEmprestados l e
-                                     laçoSubMenu4 u l e d ep
-                               0 -> laçoMenu u l e d ep
-                               _ -> laçoSubMenu4 u l e d ep
-
-
-laçoMenu :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucoes] -> [Espera] -> IO ()
+laçoMenu :: [Usuario] -> [Livro] -> [Emprestimo] -> [Devolucao] -> [Espera] -> IO ()
 laçoMenu  u l e d ep = do
                          opçao <- menu
                          case opçao of
-                           1 -> laçoSubMenu1
-                           2 -> laçoSubMenu2
-                           3 -> laçoSubMenu3
-                           4 -> laçoSubMenu4
-                           5 -> do laçoSubMenu
-                           6 -> do laçoSubMenu
-                           7 -> do laçoSubMenu
+                           1 -> laçoSubMenu1 u l e d ep
+                           2 -> laçoSubMenu2 u l e d ep
+                           3 -> laçoSubMenu3 u l e d ep
+                           4 -> laçoSubMenu4 u l e d ep
+                           5 -> laçoSubMenu1 u l e d ep
+                           6 -> laçoSubMenu1 u l e d ep
+                           7 -> laçoSubMenu1 u l e d ep
                        
