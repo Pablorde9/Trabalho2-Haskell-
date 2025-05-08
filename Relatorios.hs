1c35678c7b9f440d45f75f062a4f99f7290ff047
decolla <pedro.varise@gmail.com>
2025-05-08 12:08:24 -0300

    funcao listaEspera corrigida

--- Relatorios.hs
+++ Relatorios.hs
@@ -12,10 +12,9 @@ historicoUsuario :: String -> [Emprestimo] -> [Emprestimo]
 historicoUsuario matricula = filter (\e -> usuarioE e == matricula)
 
 -- livros com a lista de espera nao vazia e os usuarios
-livrosComEspera :: [(Int, [Usuario])] -> [Livro] -> [(Livro, [usuario])]
-livrosComEspera listaEspera livros =  [(livro, usuarios) | (idLivro, usuarios) <- listasEspera, not (null usuarios), Just livro <- [ find (\l -> idLivro l == idLivro) livros]]
+livrosComEspera :: [(Int, [Usuario])] -> [Livro] -> [(Livro, [Usuario])]
+livrosComEspera listaEspera livros =  [(livro, usuarios) | (idLiv, usuarios) <- listaEspera, not (null usuarios), Just livro <- [ find (\l -> idLivro l == idLiv) livros]]
 -- verifica se existe usuarios e procura os livros com base no id da lista de espera
--- corrigir ainda
 
 -- livros disponiveis
 livrosDisponiveis :: [Livro] -> [Emprestimo] -> [Livro]
