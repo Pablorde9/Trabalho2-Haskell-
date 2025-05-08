c60eabfccc67c032cfdb6b6ea00394de0141f0f3
decolla <pedro.varise@gmail.com>
2025-05-08 10:43:43 -0300

    as tres funções obrigatorias adicionadas

--- Relatorios.hs
+++ Relatorios.hs
@@ -1,6 +1,18 @@
 module Relatorios where
 
-
+import Data.List
 import Tipos
 
+-- lista todos emprestimos ativos (n devolvidos)
+emprestimosAtivos :: [Emprestimo] -> [Emprestimo]
+emprestimosAtivos = filter (not .devolvido )
+
+-- mostra o historico de emprestimo de um usuario especifico pela sua matricula
+historicoUsuario :: String -> [Emprestimo] -> [Emprestimo]
+historicoUsuario matricula = filter (\e -> usuarioE e == matricula)
+
+-- livros com a lista de espera nao vazia e os usuarios
+livrosComEspera :: [(Int, [Usuario])] -> [Livro] -> [(Livro, [usuario])]
+livrosComEspera listaEspera livros =  [(livro, usuarios) | (idLivro, usuarios) <- listasEspera, not (null usuarios), Just livro <- [ find (\l -> idLivro l == idLivro) livros]]
+-- verifica se existe usuarios e procura os livros com base no id da lista de espera
 
